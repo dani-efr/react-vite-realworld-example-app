4ed0fe55193322ffb34b180ed774b22af605788b
Dani Efrati <dani-e@drorisrael.org.il>
2021-09-02 10:16:12 +0300

    merging

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
